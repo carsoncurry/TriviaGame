27bc539589762ab836284e25bb42a953e2eab540
Carson Curry <carsoncurry52@gmail.com>
2018-09-13 15:17:14 -0500

    assets and index updated

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// I need to create an array of the correct answers for each question
+var answers = {
+    "one": "b",
+    "two": "c",
+    "three": "d",
+    "four": "b",
+    "five": "d",
+    "six": "a",
+    "seven": "b",
+    "eight": "c",
+    "nine": "a",
+    "ten": "a"
+}
